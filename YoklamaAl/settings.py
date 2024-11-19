4a16f47ee64087087285fb738b8f00ff79395d75
ahmet <ahmethocam@gmail.com>
2024-11-19 20:20:44 +0300

    okullar app eklendi

--- YoklamaAl/settings.py
+++ YoklamaAl/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'okullar.apps.OkullarConfig',
+    'okullar',
 ]
 
 MIDDLEWARE = [
